4804e1242c1e61f4424f7f3dbdac0904529f184f
Faruq <onaayofaruq24@gmail.com>
2024-10-23 10:48:03 -0600

    Add project files.

---
+++ ConditionalBasedCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+// TODO: Implement the calculator logic here
+double num1;
+double num2;
+
+Console.WriteLine("Enter the first number");
+bool userInput1 = double.TryParse(Console.ReadLine(), out num1);
+
+Console.WriteLine("Enter the second number");
+bool userInput2 = double.TryParse(Console.ReadLine(), out num2);
+
+
+if (userInput1 && userInput2)
+{
+    Console.WriteLine("What operation do you want to perform\n+\n-\n*\n/");
+    string operation = Console.ReadLine();
+    if (operation == "+")
+        Console.WriteLine($"Result: {num1 + num2}");
+    else if (operation == "-")
+        Console.WriteLine($"Result: {num1 - num2}");
+    else if (operation == "*")
+        Console.WriteLine($"Result: {num1 * num2}");
+    else if (operation == "/" && num2 == 0)
+        Console.WriteLine("Error: Division by zero is not allowed.");
+    else if (operation == "/")
+        Console.WriteLine($"Result: {num1 / num2}");
+    else
+    {
+        Console.WriteLine("Invalid operation. Please choose +, -, *, or /.");
+    }
+}
+
+Console.ReadLine();
